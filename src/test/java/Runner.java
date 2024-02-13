d0fa45a13f344a7897f00db60068393547dc18f7
tomas <Homero_77>
2024-02-13 03:01:13 -0300

    cambios en pom

---
+++ src/test/java/Runner.java
@@ -0,0 +1,14 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/Features",
+        glue = "test",
+        tags = "@Test",
+        plugin = {"pretty", "html:target/cucumber-reports"}
+)
+
+public class Runner {
+}
